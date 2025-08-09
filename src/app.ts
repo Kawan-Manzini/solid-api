6b0f804654f9de46a0ff9fb345910fbd30147e70
Kawan Manzini <manzinilives@gmail.com>
2025-08-09 18:18:54 -0300

    creating prisma settings with a docker postgre

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 import fastify from 'fastify';
+import { PrismaClient } from "@prisma/client";
+import { email } from 'zod';
+
 
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+prisma.user.create({
+  data: {
+    name: 'John Doe',
+    email: 'john.doe@example.com'
+  }
+})
