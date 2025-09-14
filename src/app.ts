9d10c9cd98f3b43dd3a8688eac51cde2d4c53d75
Kawan Manzini <manzinilives@gmail.com>
2025-09-14 20:00:10 -0300

    adicionado refreshTOken

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,21 @@ import { env } from './env'
 import { userRoutes } from './http/controllers/users/routes'
 import { gymRoutes } from './http/controllers/gyms/routes'
 import { checkInRoutes } from './http/controllers/check-ins/routes'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '1d'
+  }
 })
+app.register(fastifyCookie)
 
 app.register(userRoutes)
 app.register(gymRoutes)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,12 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { refreshToken } from './refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/refresh-token', refreshToken)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJWT] }, profile)
